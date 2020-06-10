914f980a9785cc6ff78a92748b83f968b5f82405
rupal11 <rupaldas2001@gmail.com>
2020-06-10 23:40:29 +0530

    Array_intro,Pointers,String_stream added to the repo.

---
+++ String_stream.cpp
@@ -0,0 +1,28 @@
+#include <sstream>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> parseInts(string str) {
+	// Complete this function
+    stringstream ss(str);
+    char ch;
+    int n;
+    vector<int> v;
+    while(ss>>n)
+    {
+      ss>>ch;
+      v.push_back(n);
+    }
+    return v;
+}
+
+int main() {
+    string str;
+    cin >> str;
+    vector<int> integers = parseInts(str);
+    for(int i = 0; i < integers.size(); i++) {
+        cout << integers[i] << "\n";
+    }
+    return 0;
+}
